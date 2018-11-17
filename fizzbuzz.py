e0ea63023922b8dad82bb13cd5071cc9832302cc
shogo <bttw.1011@gmail.com>
2018-11-17 16:45:08 +0900

    リファクタリング: ガード説を採用してみた。

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,27 +1,15 @@
 def fizzbuzz_convert(number):
     if number % 15 == 0:
-        return 'FizzBuzz'
-    elif number % 3 == 0:
-        return 'Fizz'
-    elif number % 5 == 0:
-        return 'Buzz'
-    else:
-        return str(number)
+        return "FizzBuzz"
+
+    if number % 3 == 0:
+        return "Fizz"
+
+    if number % 5 == 0:
+        return "Buzz"
+
+    return str(number)
 
 
-fizzbuzz_convert(1)
-fizzbuzz_convert(2)
-fizzbuzz_convert(3)
-fizzbuzz_convert(4)
-fizzbuzz_convert(5)
-fizzbuzz_convert(6)
-fizzbuzz_convert(7)
-fizzbuzz_convert(8)
-fizzbuzz_convert(9)
-fizzbuzz_convert(10)
-fizzbuzz_convert(11)
-fizzbuzz_convert(12)
-fizzbuzz_convert(13)
-fizzbuzz_convert(14)
-fizzbuzz_convert(15)
-
+result = fizzbuzz_convert(1)
+print(result)  # 1
